9f9fffa1176016485654811bd3bd272ef5c4cf14
Arend-Jan van Hilten <a.j.vanhilten@tudelft.nl>
2025-01-13 17:39:58 +0000

    Fix default screen

--- mirte_telemetrix_cpp/scripts/default_screen.sh
+++ mirte_telemetrix_cpp/scripts/default_screen.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 echo "IPs: "$(hostname -I)
 
 # Hostname
@@ -13,8 +13,10 @@ fi
 mirte_space=$(cat /etc/hostname | tr '[:upper:]' '[:lower:]' | tr '-' '_')
 # State of Charge
 if [ $(ros2 topic list | grep "/$mirte_space/io/power/power_watcher$") ]; then
-	soc=$(ros2 topic echo /$mirte_space/io/power/power_watcher sensor_msgs/msg/BatteryState --once --field percentage --csv --spin-time 0.000001)
-	echo SOC: $soc%
+	soc=$(ros2 topic echo /$mirte_space/io/power/power_watcher sensor_msgs/msg/BatteryState --once --field percentage --no-lost-messages --csv --spin-time 0.000001 2>/dev/null)
+	soc=$(echo "$soc * 100" | bc)
+	printf "SOC: %.0f%%\n" "$soc"
+
 fi
 
 # Time
