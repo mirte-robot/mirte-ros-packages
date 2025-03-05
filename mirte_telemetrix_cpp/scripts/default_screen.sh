648e09feaa38bd560474c5be80708e079967cab4
Arend-Jan van Hilten <a.j.vanhilten@tudelft.nl>
2025-03-05 17:49:17 +0100

    Improve style default_screen

--- mirte_telemetrix_cpp/scripts/default_screen.sh
+++ mirte_telemetrix_cpp/scripts/default_screen.sh
@@ -1,29 +1,27 @@
 #!/bin/bash
 # set -x
-echo "IPs: "$(hostname -I)
+echo "IPs: $(hostname -I)"
 
 # Hostname
-echo "Hn:"$(cat /etc/hostname)
+echo "Hn:$(cat /etc/hostname)"
 
 # Wi-Fi Line
 wifi=$(iwgetid -r)
 if [ "$wifi" ]; then
 	echo Wi-Fi: $wifi
 fi
-# echo $MIRTE_USE_MULTIROBOT
-topic="/io/power/power_watcher"
-if [ "$MIRTE_USE_MULTIROBOT" = 'true' ]; then
-	mirte_space=$(cat /etc/hostname | tr '[:upper:]' '[:lower:]' | tr '-' '_')
-	topic="/$mirte_space$topic"
-fi
 
-# State of Charge
-if [ $(ros2 topic list | grep "$topic$") ]; then
-	soc=$(ros2 topic echo $topic sensor_msgs/msg/BatteryState --once --field percentage --no-lost-messages --csv --spin-time 0.000001 2>/dev/null)
-	soc=$(echo "$soc * 100" | bc)
+# just assume that the battery is at /tmp/batteryState, printed by the mirte_master_check script
+# way faster than using ros2 topic echo
+percentage=$(
+	tail -2 /tmp/batteryState | head -1
+) || true
+
+if [ "$(echo $percentage | wc -c)" -gt 1 ]; then
+	percentage=$(echo "$percentage" | awk '{print $NF}')
+	soc=$(echo "$percentage * 100" | bc)
 	printf "SOC: %.0f%%\n" "$soc"
-
 fi
 
 # Time
-echo "Time: "$(date +"%H:%M:%S")
+echo "Time: $(date +"%H:%M:%S")"
