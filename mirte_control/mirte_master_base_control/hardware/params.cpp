86f12545260dc2a873c22963b88055a004c32fbf
Arend-Jan van Hilten <a.j.vanhilten@tudelft.nl>
2025-10-03 14:40:24 +0200

    Add single client optional mirte master controller

--- mirte_control/mirte_master_base_control/hardware/params.cpp
+++ mirte_control/mirte_master_base_control/hardware/params.cpp
@@ -7,4 +7,12 @@ void parse_params(std::string param_file, std::shared_ptr<rclcpp::Node> nh) {
   nh->declare_parameter("wheel_diameter",
                         config["wheel_diameter"].as<double>());
   nh->declare_parameter("max_speed", config["max_speed"].as<double>());
+
+  // check if config has the key
+  if (config["single_client"]) {
+    nh->declare_parameter<bool>("single_client",
+                               config["single_client"].as<bool>());
+  } else {
+    nh->declare_parameter<bool>("single_client", true);
+  }
 }
