e018794b4f7ba60d275171c7eb1942d8509fc9d7
Arend-Jan van Hilten <a.j.vanhilten@tudelft.nl>
2026-04-02 16:04:24 +0200

    Fix servo-name hiwonder

--- mirte_telemetrix_cpp/src/modules/hiwonder_module.cpp
+++ mirte_telemetrix_cpp/src/modules/hiwonder_module.cpp
@@ -195,7 +195,7 @@ HiWonderBus_module::get_hiwonder_modules(
 
           std::set<std::string> unused_keys = get_keys(parameters);
           std::shared_ptr<HiWonderServoData> data =
-              std::make_shared<HiWonderServoData>(parser, node_data.board, name,
+              std::make_shared<HiWonderServoData>(parser, node_data.board, servo_name,
                                                   parameters, unused_keys, "");
           motor_data.push_back(data);
         }
