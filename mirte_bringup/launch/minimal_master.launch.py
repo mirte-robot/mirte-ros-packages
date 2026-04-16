b3175a76b38cd1b0244cfbc24253f8533846240c
Arend-Jan van Hilten <a.j.vanhilten@tudelft.nl>
2026-04-16 17:27:10 +0200

    Add configurable ticks and invert motors

--- mirte_bringup/launch/minimal_master.launch.py
+++ mirte_bringup/launch/minimal_master.launch.py
@@ -15,6 +15,9 @@ from launch_ros.substitutions import FindPackageShare
 from launch_ros.actions import Node, PushRosNamespace, SetRemap
 from pathlib import Path
 
+ticks = 1321
+invert_motors = False
+
 
 def generate_launch_description():
     ld = LaunchDescription(
@@ -51,7 +54,7 @@ def generate_launch_description():
     use_base_pid_control = LaunchConfiguration(
         "use_base_pid_control",
     )
-
+    ticks_arg = LaunchConfiguration("ticks", default=str(ticks))
     telemetrix = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
             [
@@ -64,17 +67,32 @@ def generate_launch_description():
                 )
             ]
         ),
-        launch_arguments={
-            "config_path": PathJoinSubstitution(
-                [
-                    FindPackageShare("mirte_bringup"),
-                    "telemetrix_config",
-                    "mirte_master_config.yaml",
-                ]
-            ),
-            "hardware_namespace": hardware_namespace,
-            "frame_prefix": frame_prefix,
-        }.items(),
+        launch_arguments=(
+            {
+                "config_path": PathJoinSubstitution(
+                    [
+                        FindPackageShare("mirte_bringup"),
+                        "telemetrix_config",
+                        "mirte_master_config.yaml",
+                    ]
+                ),
+                "hardware_namespace": hardware_namespace,
+                "frame_prefix": frame_prefix,
+            }
+            | (
+                {
+                    "overlay_config_path": PathJoinSubstitution(
+                        [
+                            FindPackageShare("mirte_bringup"),
+                            "telemetrix_config/overlay",
+                            "200rpm_wheels.yaml",
+                        ]
+                    )
+                }
+                if invert_motors
+                else {}
+            )
+        ).items(),
     )
     ros2_control = GroupAction(
         actions=[
@@ -127,7 +145,7 @@ def generate_launch_description():
                 )
             ]
         ),
-        launch_arguments={"frame_prefix": frame_prefix}.items(),
+        launch_arguments={"frame_prefix": frame_prefix, "ticks": ticks_arg}.items(),
     )
 
     mecanum_drive_control = IncludeLaunchDescription(
@@ -195,14 +213,10 @@ def generate_launch_description():
         output="screen",
     )
 
-    
     # use lsusb to detect which depth cam is connected
-    astra_devices = [
-        device.name
-        for device in Path("/dev").glob("astra*")
-    ]
+    astra_devices = [device.name for device in Path("/dev").glob("astra*")]
     print(astra_devices)
-    if("astradepth" in astra_devices and "astrauvc" in astra_devices):
+    if "astradepth" in astra_devices and "astrauvc" in astra_devices:
         depth_cam = GroupAction(
             actions=[
                 # SetRemap(src="/camera/color/image_raw", dst="/camera/color/_image_raw"),
@@ -223,7 +237,7 @@ def generate_launch_description():
                 ),
             ]
         )
-    elif("astra_mini_pro" in astra_devices):
+    elif "astra_mini_pro" in astra_devices:
         depth_cam = GroupAction(
             actions=[
                 IncludeLaunchDescription(
@@ -233,7 +247,7 @@ def generate_launch_description():
                                 [
                                     FindPackageShare("orbbec_camera"),
                                     "launch",
-                                    "astra.launch.py", # for the astra mini S pro
+                                    "astra.launch.py",  # for the astra mini S pro
                                 ]
                             )
                         ]
@@ -277,7 +291,9 @@ def generate_launch_description():
                 cameras,
                 web_video_server,
                 lidar,
-                *([depth_cam] if depth_cam is not None else []), # if no cam is found, just not start it as it is unknown which one to start.
+                *(
+                    [depth_cam] if depth_cam is not None else []
+                ),  # if no cam is found, just not start it as it is unknown which one to start.
                 arm_control,
                 mecanum_drive_control,
                 rosbridge,
